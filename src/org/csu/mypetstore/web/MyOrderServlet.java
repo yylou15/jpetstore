7f78d78374646a2af00559f9d72b0f30fbe1ac8f
no_wonder <m13278885501@163.com>
2018-12-13 21:41:19 +0800

    改了account登陆和注册的问题，order的添加还没实现

--- src/org/csu/mypetstore/web/MyOrderServlet.java
+++ src/org/csu/mypetstore/web/MyOrderServlet.java
@@ -20,6 +20,7 @@ public class MyOrderServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session=request.getSession();
+        order=(Order)session.getAttribute("order");
 
         request.getRequestDispatcher(MY_ORDER).forward(request,response);
     }
